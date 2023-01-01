060c2fb7e0a964eef3aa14bb44b4aa857ca55f9e
pholmq <pholmq@gmail.com>
2023-01-01 12:23:26 +0100

    camera, stars, orbitcontrols

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,23 @@
 
 import type { NextPage } from "next";
 import { Canvas } from "@react-three/fiber"
+import { Stats, OrbitControls, Stars } from "@react-three/drei";
 
 const Home: NextPage = () => {
   return (
     <div className="container">
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-
+      <Canvas
+          camera={{
+            fov: 15,
+            position: [0, 3000, 0],
+            near: 0.1,
+            far: 10000000
+          }}
+      >
+          <OrbitControls makeDefault enableDamping={false} maxDistance={500000} />
+          <axesHelper args={[10, 10, 10]} position={[0, 0, 0]} />
+          <ambientLight intensity={0.5} />
+          <Stars radius={100000} />
       </Canvas>
     </div>
   )
